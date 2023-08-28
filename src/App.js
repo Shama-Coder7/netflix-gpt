af99580c8dc169f2df5f7ef749a7e003c4bb0222
ShamVeen <118723873+ShamVeen@users.noreply.github.com>
2023-08-29 02:34:05 +0530

    changed header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold text-green-800">
-        Hello Shama, You have to do it , don't disappont. you have to proud your
-        parents.
+        Hello Shama! You have to do it , don't disappoint. you have to proud your
+        parents. You will make it. Inshaallah.
       </h1>
     </div>
   );
